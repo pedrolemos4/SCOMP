61e8463906cee94f3ba1986696579d63b12ba247
Tiago_1191098 <1191098@isep.ipp.pt>
2021-03-18 17:05:13 +0000

    Exercício 5

---
+++ PL_01/1191098-ex05/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(){
+	pid_t p;
+	int status;
+	int valor;
+	
+	int i;
+	for(i=0;i<2;i++){
+		p = fork();
+		if(p<=0){
+			sleep(1);
+			exit(1);
+		}
+	}
+	
+	for(i=0;i<2;i++){
+		wait(&status);
+		valor = WEXITSTATUS(status);
+		printf("%d\n",valor);
+	}
+	return 0;
+}
