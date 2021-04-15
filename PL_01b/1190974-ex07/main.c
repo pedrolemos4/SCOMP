cd4bac9f3c6914e0ca508b4870ac689103efe4db
Pedro <1190974@isep.ipp.pt>
2021-04-15 19:31:43 +0100

    exe8

--- PL_01b/1190974-ex07/main.c
+++ PL_01b/1190974-ex07/main.c
@@ -1,43 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <sys/wait.h>
+#include <signal.h>
 #include <string.h>
-#include <sys/types.h>
+#include <stdio.h>
 
-void handle_SIGUSR1 (int signo, siginfo_t *sinfo, void *context){
-	
-	sigset_t mask, pending;
-	
-	sigfillset(&mask);
-	sigaddset(&mask,SIGUSR1);
-	sigprocmask(SIG_BLOCK, NULL, 0);
-	
-	sigpending(&pending);
-	if(sigismember(&pending, SIGUSR1)){
-		printf("SIGUSR1 pending\n");
-	}
-	
-	for(;;){
-		printf("handle_SIGUSR1\n");
-		sleep(1);
-	}
-	
-	sigprocmask(SIG_UNBLOCK, &mask, 0);
+struct sigaction act;
+volatile sig_atomic_t i = 1;
+    void signals_set(sigset_t * set)
+{
+    sigprocmask(SIG_SETMASK, NULL, set);
+    while (i<=65)  { 
+        if (sigismember(set, i) == 1){
+            char c[27];
+            sprintf(c, "O sinal %d está bloqueado.\n", i);
+            write(STDOUT_FILENO, c, 28);
+        } else{
+            char c[31];
+            sprintf(c, "O sinal %d não está bloqueado.\n", i);
+            write(STDOUT_FILENO, c, 34);
+        }
+        ++i;
+    }
 }
 
-int main() {
-	
-	struct sigaction act;
-	memset(&act, 0, sizeof(struct sigaction));
-	sigemptyset(&act.sa_mask); 
-	act.sa_sigaction = handle_SIGUSR1;
-	sigaction(SIGUSR1, &act, NULL);
-	
-	for(;;){
-		printf("Waiting for SIGUSR1...\n");
-		sleep(1);
-	}
-	
-	return 0;
+void handle_signal(int sign, siginfo_t *sinfo, void *context){
+    signals_set(&act.sa_mask);
+}
+
+int main(){
+    memset(&act, 0, sizeof(struct sigaction));
+    sigfillset(&act.sa_mask);
+    act.sa_sigaction = handle_signal;
+    act.sa_flags = SA_SIGINFO;
+    sigaction(SIGUSR1, &act, NULL);
+    raise(SIGUSR1); 
+    return 0;
 }
---
+++ PL_02/1190974-ex13/main.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>	   
+#include <sys/types.h> 
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+
+	int fd[4][2];
+	pid_t pid;
+	int amount_pieces=1001, i, k, amount = 0, final=0, inventario=0;
+	
+	for(i = 0; i < 4; i++){
+		if(pipe(fd[i]) == -1){
+			perror("Pipe failed");
+			return 1;
+		}
+	}
+	
+	for(i=0;i<4;i++){
+		pid = fork();
+		 if(pid==0){
+			if(i==0){	//M1
+				close(fd[i+1][0]);
+				for(k=1;k<amount_pieces;k++){
+					if(k%5==0){
+						printf("0 Foram cortadas e transferidas %d peças. \n",k);
+						write(fd[i+1][1],&k, sizeof(int));
+					}
+				}
+				close(fd[i+1][1]);
+				exit(0);
+			}else{
+				if(i==1){	//M2
+					close(fd[i][1]);
+					close(fd[i+1][0]);
+					while((read(fd[i][0],&amount,sizeof(int)))>0){
+						if(amount%5==0){
+							write(fd[i+1][1],&amount, sizeof(int));
+							printf("1 Foram dobradas e transferidas %d peças. \n",amount);
+						}
+					}
+					close(fd[i][0]);
+					close(fd[i+1][1]);
+					exit(0);
+				}else{
+					if(i==2){	//M3
+						close(fd[i][1]);
+						close(fd[i+1][0]);
+						while((read(fd[i][0],&amount,sizeof(int)))>0){
+							if(amount%10==0){
+								write(fd[i+1][1],&amount, sizeof(int));
+								printf("2 Foram fundidas e transferidas %d peças. \n",amount);
+							}
+						}
+						close(fd[i][0]);
+						close(fd[i+1][1]);
+						exit(0);
+					}else{
+						if(i==3){	//M4
+							close(fd[i][1]);
+							close(fd[0][0]);
+							while((read(fd[i][0],&amount,sizeof(int)))>0){
+								if(amount%100==0){
+									write(fd[0][1],&amount, sizeof(int));
+									printf("3 Foram armazenadas e adicionadas a stock %d peças. \n",amount);
+								}
+							}
+							close(fd[i][0]);
+							close(fd[0][1]);
+							exit(0);
+						}
+					}						
+				}
+			}
+			exit(0);
+		}
+	}
+	close(fd[0][1]);
+	while((read(fd[0][0],&final,sizeof(int)))>0){
+		inventario=final;
+		printf("Inventário %d. \n", inventario);
+	}
+	close(fd[0][0]);
+	printf("Inventário final %d. \n",inventario);
+	
+	return 0;
+}
