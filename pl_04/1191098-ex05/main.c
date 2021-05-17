2df24d9bba8dff147e225d804a3b90227fc3c368
Tiago_1191098 <1191098@isep.ipp.pt>
2021-05-17 23:37:37 +0100

    Exercício 5

---
+++ pl_04/1191098-ex05/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <semaphore.h>
+#include <string.h>
+
+
+int main(){
+
+	sem_t *semFilho;
+	sem_t *semPai;
+
+	if ((semFilho = sem_open("semex5Filho", O_CREAT | O_EXCL, 0644,1)) == SEM_FAILED) {
+		perror("Error in sem_open()");
+		exit(1);
+	}
+	
+	if ((semPai = sem_open("semex5Pai", O_CREAT | O_EXCL, 0644,0)) == SEM_FAILED) {
+		perror("Error in sem_open()");
+		exit(1);
+	}
+
+	pid_t p;
+	
+	p= fork();
+	
+	if(p>0){
+		sem_wait(semPai);
+		
+		printf("I'm the father.\n");
+		
+		sem_post(semPai);
+		
+	} else{
+		
+		sem_wait(semFilho);
+		
+		printf("I'm the child.\n");
+		
+		sem_post(semPai);
+		sem_post(semFilho);
+		
+	}
+	
+	sem_unlink("semex5Filho");
+	sem_unlink("semex5Pai");
+		
+	
+	return 0;
+}
